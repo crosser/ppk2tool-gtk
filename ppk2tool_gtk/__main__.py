03c7962b90ce098b843f34211c197f19b9717261
Eugene Crosser <crosser@average.org>
2026-07-14 19:59:07 +0200

    Monitor device appearance / disappearance

--- ppk2tool_gtk/__main__.py
+++ ppk2tool_gtk/__main__.py
@@ -14,7 +14,7 @@ gi.require_version("Gdk", "4.0")
 gi.require_version("Gtk", "4.0")
 gi.require_version("Graphene", "1.0")
 gi.require_version("Pango", "1.0")
-from gi.repository import Gdk, GLib, Gtk, Adw, Graphene, Pango
+from gi.repository import Adw, Gdk, Gio, GLib, Gtk, Graphene, Pango
 
 from ppk2tool import *
 
@@ -87,16 +87,29 @@ class MainWindow(Gtk.ApplicationWindow):
         box.append(graph := Graph())
         print("Added graph object", graph)
 
+        devmon = Gio.File.new_for_path("/dev").monitor_directory(
+            Gio.FileMonitorFlags.NONE
+        )
+        devmon.connect("changed", self.on_devchange, None)
+        if self.findppk():
+            print("PPK2 initialised")
+        else:
+            print("No PPK2")
+
+    def findppk(self) -> bool:
         found = 0
         devname = None
-        for e in os.listdir("/dev/serial/by-id"):
-            if e.startswith("usb-Nordic_Semiconductor_PPK2"):
-                found += 1
-                devname = e
+        try:
+            for e in os.listdir("/dev/serial/by-id"):
+                if e.startswith("usb-Nordic_Semiconductor_PPK2"):
+                    found += 1
+                    devname = e
+        except FileNotFoundError:
+            pass
         if found != 1:
             print("zero or more than one profiler devices")
-            self.close()
-            exit(1)
+            self.ppk = None
+            return False
         assert devname is not None, "listdir() returned entry None?!"
         devpath = os.path.join("/dev/serial/by-id", devname)
         print("Using PPK on", devpath)
@@ -104,6 +117,21 @@ class MainWindow(Gtk.ApplicationWindow):
         self.ppk = PPK2Source(devpath, self.on_ppk_result)
         print("Registered source", self.ppk)
         self.ppk.attach(GLib.MainContext.default())
+        return true
+
+    def on_devchange(
+        self,
+        fmon: Gio.FileMonitor,
+        file: Gio.File,
+        other: Gio.File,
+        evtype: Gio.FileMonitorEvent,
+        _: Literal[None],
+    ):
+        print("dev event", fmon, file, other, evtype)
+        if self.findppk():
+            print("PPK2 initialised")
+        else:
+            print("No PPK2")
 
     def on_keypress(
         self,
@@ -118,7 +146,8 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def hello(self, button):
         print("Clicked", button)
-        self.ppk.send(PPK2Cmd.GET_META_DATA)
+        if self.ppk:
+            self.ppk.send(PPK2Cmd.GET_META_DATA)
 
     def on_ppk_result(self, cmd: PPK2Cmd, data: PPK2Meta | PPK2Sample) -> None:
         if isinstance(data, PPK2Meta):
