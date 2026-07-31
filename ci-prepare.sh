efc5058f6ef89164442842bd04265eb75e894e5f
Eugene Crosser <crosser@average.org>
2026-07-31 18:11:02 +0200

    make ci-prepare work (?)

--- ci-prepare.sh
+++ ci-prepare.sh
@@ -4,10 +4,11 @@ set -e
 # Script to run inside CI build container to install dependency
 
 URL=$(curl -s https://api.github.com/repos/crosser/ppk2tool/releases/latest \
- | jq -r '.assets[]|select(.content_type == "application/x-debian-package") \
+ | jq -r '.assets[]|select(.content_type == "application/x-debian-package")
  |.browser_download_url')
 PKG=$(basename $URL)
 
-trap "rm -f $PKG" EXIT
-curl --remote-name $URL
-apt install -y ./$PKG
+TMP=$(mktemp -d)
+trap "rm -rf $TMP" EXIT
+curl --follow --output $TMP/$PKG $URL
+apt install -y $TMP/$PKG
