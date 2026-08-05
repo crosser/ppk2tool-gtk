3f8190a418ddbaa6de82852c55e7da160ca943fa
Eugene Crosser <crosser@average.org>
2026-08-05 22:16:15 +0200

    Install curl and jq in the container before use

--- ci-prepare.sh
+++ ci-prepare.sh
@@ -3,6 +3,9 @@ set -e
 
 # Script to run inside CI build container to install dependency
 
+apt-get update
+apt-get install -y curl jq
+
 URL=$(curl -s https://api.github.com/repos/crosser/ppk2tool/releases/latest \
  | jq -r '.assets[]|select(.content_type == "application/x-debian-package")
  |.browser_download_url')
